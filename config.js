f15cdc45638945525af582b581539a0dcedef8fc
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-08-15 00:27:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/ZKjzjwr.jpeg" ,
+ALIVE_MSG: process.env.ALIVE_MSG || "Hey, i am  HORIZON-MD Whats app bot i am alive now!"
 };
