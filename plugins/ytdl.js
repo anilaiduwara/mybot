5602d52808420a3935c1c394b62211fcc421e396
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-08-17 15:03:16 +0530

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -35,7 +35,6 @@ let downloadUrl down.dl_url
 await.conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
 await.conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp4",caption: 💻MADE BY ANILALOCHANA💻 },{quoted:mek})
 
-
   
 }catch(e){
 console.log(e)
