4d1e74b2b4087ec6046897ac547017cbce47f5b0
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-10-12 12:34:07 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -4,7 +4,7 @@ const {cmd , commands} = require('../command')
 cmd({
     pattern: "alive",
     desc: "Check bot online or no.",
-    react: "🎞️"
+    react: "🎞️",
     category: "main",
     filename: __filename
 },
