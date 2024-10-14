17d363ff48181ea5fd93cee333ecbb59fad67044
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-10-14 09:26:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3f7249eb429c8211cbba3.jpg" ,
-ALIVE_MSG: process.env.ALIVE_MSG || "Hey, i am  BLACK-LEAGUE-MD Whats app bot i am alive now!" ,
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true" ,
-MODE: process.env.MODE || "public",
 MONGODB: process.env.MONGODB || "",
 
 };
