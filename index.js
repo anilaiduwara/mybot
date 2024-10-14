eeea43b41ee6adf94433738c680df678d0eee641
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-10-14 09:35:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const port = process.env.PORT || 8000;
 //=============================================
 async function connectToWA() {
 //=====connect mongodb=========
-const connectDB =(`./lib/mongodb`)
+const connectDB =require(`./lib/mongodb`)
 connectDB();
       
  //======================
