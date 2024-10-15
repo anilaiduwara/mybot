bfed246c6c57cfe5682265f81626896c994a01fa
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-10-15 16:51:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "hQoFkZZJ#cUMLT8HqNr-vhK-6rBQcY9Zx7Ti1jaCrHLckQDsrpgM",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 
 };
