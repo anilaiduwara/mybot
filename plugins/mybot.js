61d2fc29d51ba9f4617c024cc0fed8b5dd1c4d57
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-10-16 08:46:00 +0530

    Update mybot.js

--- plugins/mybot.js
+++ plugins/mybot.js
@@ -1,5 +1,5 @@
 const config = require('../lib/functions')
-const {cmd,commands} = require('../Command')
+const {cmd,commands} = require('../commands')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson, jsonformat} = require('../lib/functions')
 
 
