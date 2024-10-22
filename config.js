e80c410d38c70222f100117d7137281737669a06
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-10-22 23:05:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 
 };
